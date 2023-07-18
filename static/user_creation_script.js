a029337768f1a02709a232c9962b592fd7f9ed03
thx4nothing <thx4nothing15@gmx.de>
2023-07-18 23:09:43 +0900

    added basic database and user creation. user get redirected to a new session after creation

---
+++ static/user_creation_script.js
@@ -0,0 +1,38 @@
+document.getElementById("userForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get form values
+    var first_name = document.getElementById("firstName").value;
+    var last_name = document.getElementById("lastName").value;
+    var age = document.getElementById("age").value;
+
+    // Create JSON object with user information
+    var userInfo = {
+        first_name: first_name,
+        last_name: last_name,
+        age: age
+    };
+
+    fetch("/create_user", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userInfo)
+    })
+    .then(response => {
+        if (response.redirected) {
+            // Get the redirect URL from the response headers
+            const redirectUrl = response.url;
+
+            // Manually redirect the user to the new URL
+            window.location.href = redirectUrl;
+        }
+    })
+    .catch(error => {
+        console.error("Error creating user:", error);
+    });
+
+    // Reset the form
+    document.getElementById("userForm").reset();
+});
